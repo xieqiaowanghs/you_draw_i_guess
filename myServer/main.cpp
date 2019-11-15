696fe37ed9c21019688db4829f0b5565cd7bd40c
xieqiaowanghs <1003505440@qq.com>
2019-11-15 22:50:18 +0800

    Update main.cpp

--- myServer/main.cpp
+++ myServer/main.cpp
@@ -7,6 +7,6 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     Server s;
-    s.initServer("192.168.1.227",7000);
+    s.initServer("127.0.0.1",7000);
     return a.exec();
 }
